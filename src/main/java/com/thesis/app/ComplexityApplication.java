f2c55d237e484014a30ca10944c9e2a0871fd151
ikljenak <ikljenak.2@gmail.com>
2017-11-18 11:43:40 +0100

    more changes

--- src/main/java/com/thesis/app/ComplexityApplication.java
+++ src/main/java/com/thesis/app/ComplexityApplication.java
@@ -5,26 +5,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
 import com.thesis.app.models.Container;
 import com.thesis.app.models.Item;
 import com.thesis.app.models.Result;
-import com.thesis.app.models.SolutionBrute;
 import com.thesis.app.utils.BoxHelper;
 import com.thesis.app.utils.Configuration;
 
 public class ComplexityApplication {
 
-	private static final List<Item> items = new LinkedList<Item>();
 	private static long[] time;
 	private static int itemsPacked;
 
 	public static void main(String[] args) throws IOException {
 		initialConfiguration(args[0]);
 		time = new long[Configuration.ITERATIONS_NUMBER];
+		BoxHelper.getInstance();
 		for (int i = 0; i < Configuration.ITERATIONS_NUMBER; i++) {
 			Result result = run();
 			time[i] = result.getTime();
@@ -36,15 +34,14 @@ public class ComplexityApplication {
 	}
 
 	public static Result run() throws IOException {
-		initializeItems();
+		List<Item> items = initializeItems();
 		long startTime = System.currentTimeMillis();
-
 		List<Container> containers = new ArrayList<Container>();
 		Container c = BoxHelper.getInstance().getBox(
 				Configuration.CONTAINERS_NUMBER - 1);
 		containers.add(c.copy());
-		SolutionBrute solution = new SolutionBrute(containers);
 
+		int itemsPacked = 0;
 		for (Item item : items) {
 			int index = 0;
 			while (index <= containers.size()) {
@@ -54,7 +51,7 @@ public class ComplexityApplication {
 
 				Container container = containers.get(index);
 				if (container.pack(item)) {
-					solution.setItemsPacked(solution.getItemsPacked() + 1);
+					itemsPacked++;
 					break;
 				} else {
 					index++;
@@ -64,18 +61,20 @@ public class ComplexityApplication {
 
 		long endTime = System.currentTimeMillis();
 		return new Result(endTime - startTime, 0, 0, 0,
-				solution.getItemsPacked());
+				itemsPacked);
 	}
 
-	private static void initializeItems() throws IOException {
+	private static List<Item> initializeItems() throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader(
 				Configuration.INPUT_FILE));
+		List<Item> items = new ArrayList<Item>();
 		for (int i = 0; i < Configuration.INPUT_SIZE; i++) {
 			String[] values = in.readLine().split(" ");
 			items.add(new Item(new Double(values[0]), new Double(values[1]),
 					new Double(values[2]), new Double(values[3])));
 		}
 		in.close();
+		return items;
 	}
 
 	private static void initialConfiguration(String propertiesFile) throws IOException {
